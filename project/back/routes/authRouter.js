49f066344e7193b4eeb4259ca2aeec8bc9e335a9
dron512 <parkmyounghoi@gmail.com>
2023-05-10 13:31:21 +0900

    preprocessing

--- project/back/routes/authRouter.js
+++ project/back/routes/authRouter.js
@@ -4,34 +4,51 @@ const bcrypt = require('bcrypt');
 const {User} = require('../db.js');
 const jwt = require('jsonwebtoken');
 
-const mypassword = 'a123456789';
+const secretKey = 'a123456789';
+
+router.post('/login',async(req,res)=>{
+  try{
+    const result = await jwt.verify(req.body.mytoken, secretKey);
+    console.log(result);
+    return res.send('login');
+  }catch(error){
+    if (error.name === 'TokenExpiredError') { // 유효기간 초과
+      return res.status(419).json({
+        code: 419,
+        message: '토큰이 만료되었습니다',
+      });
+    }
+    return res.status(401).json({
+      code: 401,
+      message: '유효하지 않은 토큰입니다',
+    });
+  }
+})
 
 router.post('/signin', async (req, res) => {
-  console.log(req.body.email);
-  console.log(req.body.password);
-
   const oneUser = await User.findOne({
     where: {
       email: req.body.email,
     },
   });
-
+  if (oneUser === null) {
+    return res.send({code: 418, message: '해당하는 User가 없습니다'});
+  }
+  const payload = {name: oneUser.name, email: oneUser.email};
   try {
     const comp = await bcrypt.compare(req.body.password, oneUser.password);
     if (comp) {
-      jwt.sign(payload, secret, {expiresIn: '1m'}, (err, token) => {
-        if (err instanceof jwt.TokenExpiredError) {
-          // JWT의 유효기간이 만료된 경우
-          return res
-            .status(401)
-            .send('Token expired. Please renew your token.');
-        }
-      });
+      const token = jwt.sign(payload, secretKey, {expiresIn: '1m'});
+      res.json({code: 200, message: '토근이발급되었습니다.', token});
     } else {
-      res.json(419);
+      return res.send({code: 419, message: '비밀번호가 다릅니다.'});
     }
   } catch (e) {
     console.log(e);
+    return res.status(500).json({
+      code: 500,
+      message: '서버 에러',
+    });
   }
 });
 
